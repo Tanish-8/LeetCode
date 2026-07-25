abea3a22a3c1ff7e219c2009cd268ee5ca2592c3
Tanish <madishettytanish@gmail.com>
2026-07-25 15:22:29 +0530

    Time: 4 ms (26.61%), Space: 12.4 MB (17.34%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -4,11 +4,25 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        x=0
-        sum=0
-        for s in str(n):
-            if s!='0':
-                i=int(s)
-                x=x*10+i
-                sum+=i
-        return x*sum
+        x=""
+        y=""
+        s=0
+        n=str(n)
+        i,j=0,len(n)-1
+        while i<=j:
+            if i==j:
+                if n[i]!='0':
+                    x+=n[i]
+                    s+=int(n[i])
+                break
+            if n[i]!='0':
+                x+=n[i]
+                s+=int(n[i])
+            if n[j]!='0':
+                y+=n[j]
+                s+=int(n[j])
+            i+=1
+            j-=1
+        if not x and not y:
+            return 0
+        return int(x+y[::-1])*s
