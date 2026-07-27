dbab20c541b484790ad56bf269fd284649175188
Tanish <madishettytanish@gmail.com>
2026-07-27 12:03:02 +0530

    Time: 3 ms (69.09%), Space: 23.3 MB (31.31%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,4 +6,6 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         k%=len(nums)
-        nums[:]=nums[-k:]+nums[:-k]
+        nums.reverse()
+        nums[:k]=reversed(nums[:k])
+        nums[k:]=reversed(nums[k:])
