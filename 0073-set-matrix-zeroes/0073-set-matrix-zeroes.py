8a772278a3a4ead11ea96f43a31d43711a946b4e
Tanish <madishettytanish@gmail.com>
2026-08-04 20:27:05 +0530

    Time: 14 ms (39.95%), Space: 13.9 MB (8.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,17 +4,30 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        rows=set()
-        cols=set()
         m=len(matrix)
         n=len(matrix[0])
+        f_row=False
+        f_col=False
         for r in range(m):
-            for c in range(n):
+            if matrix[r][0]==0:
+                f_col=True
+                break
+        for c in range(n):
+            if matrix[0][c]==0:
+                f_row=True
+                break
+        for r in range(1,m):
+            for c in range(1,n):
                 if matrix[r][c]==0:
-                    rows.add(r)
-                    cols.add(c)
-        for r in range(m):
-            for c in range(n):
-                if r in rows or c in cols:
+                    matrix[r][0]=0
+                    matrix[0][c]=0
+        for r in range(1,m):
+            for c in range(1,n):
+                if matrix[r][0]==0 or matrix[0][c]==0:
                     matrix[r][c]=0
-        
+        if f_row:
+            for c in range(n):
+                matrix[0][c]=0
+        if f_col:
+            for r in range(m):
+                matrix[r][0]=0
